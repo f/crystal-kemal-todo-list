2528fde22695297b87ac76dbdae723b658863671
Fatih Kadir Akın <fakin@protel.com.tr>
2015-12-05 15:05:55 +0200

    update

--- public/index.js
+++ public/index.js
@@ -1,7 +1,8 @@
-# Utils
-function eventBus = $({});
+// Utils
+var eventBus = $({});
 
 function request(method, url, params) {
+  console.log(arguments);
   return $.ajax({
     method: method,
     url: url,
@@ -10,7 +11,7 @@ function request(method, url, params) {
   });
 }
 
-# Todo Model
+// Todo Model
 function Todo(id, text, isCompleted) {
   this.id = id;
   this.text = text;
@@ -18,31 +19,31 @@ function Todo(id, text, isCompleted) {
 };
 
 Todo.prototype.delete = function () {
-  request("delete", "/todos/" + this.id).done(function () {
+  return request("delete", "/todos/" + this.id).done(function () {
     eventBus.trigger("delete", this);
   });
 };
 
 Todo.prototype.create = function () {
-  request("create", "/todos/").done(function (newTodo) {
+  return request("post", "/todos/", {text: this.text}).done(function (newTodo) {
     eventBus.trigger("create", newTodo.id);
   });
 };
 
 Todo.prototype.complete = function () {
-  request("patch", "/todos/" + this.id).done(function () {
+  return request("patch", "/todos/" + this.id).done(function () {
     eventBus.trigger("complete", this);
   });
 };
 
-# TodoView
+// TodoView
 
 function TodoView(model) {
   this.$el = $("<li/>");
   this.$el.html(model.text);
   this.$el.data('model', model);
-  
-  var checker = $("<input type=checkbox/>");
+
+  var checker = $("<input type='checkbox'/>");
   checker.on("click", function () {
     var check = checker.is(":checked");
     if (check) {
@@ -50,7 +51,7 @@ function TodoView(model) {
     }
   });
   this.$el.prepend(checker);
-  
+
   eventBus.on("complete", function (e, data) {
     if (data === model) {
       this.$el.fadeOut(100).remove();
@@ -58,14 +59,23 @@ function TodoView(model) {
   });
 }
 
-# Main
+// Main
 
 (function main() {
   request("get", "/todos").done(function (todos) {
     todos.forEach(function (todo) {
       var model = new Todo(todo.id, todo.text, todo.isCompleted);
       var view = new TodoView(model);
-      $("#todos").append(view);
+      $("#todos").append(view.$el);
+    });
+  });
+
+  $("#add_todo").click(function () {
+    var model = new Todo(null, $("#new_todo").val());
+    model.create().done(function (data) {
+
+      var view = new TodoView(model);
+      $("#todos").append(view.$el);
     });
   });
 })();
